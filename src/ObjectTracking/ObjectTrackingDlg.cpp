3d37a416e48506e83101231eb5b721dea2728d1b
DESKTOP-MHAHMVM\lhu <wjs7347@gmail.com>
2022-02-11 10:55:30 +0900

    - model load path (abs path->relative path)

--- src/ObjectTracking/ObjectTrackingDlg.cpp
+++ src/ObjectTracking/ObjectTrackingDlg.cpp
@@ -48,8 +48,6 @@ BOOL CObjectTrackingDlg::OnInitDialog()
 {
 	CDialogEx::OnInitDialog();
 
-	// 이 대화 상자의 아이콘을 설정합니다.  응용 프로그램의 주 창이 대화 상자가 아닐 경우에는
-	//  프레임워크가 이 작업을 자동으로 수행합니다.
 	SetIcon(m_hIcon, TRUE);			// 큰 아이콘을 설정합니다.
 	SetIcon(m_hIcon, FALSE);		// 작은 아이콘을 설정합니다.
 
@@ -68,22 +66,17 @@ BOOL CObjectTrackingDlg::OnInitDialog()
 	}
 
 
-	return TRUE;  // 포커스를 컨트롤에 설정하지 않으면 TRUE를 반환합니다.
+	return TRUE;
 }
-
-// 대화 상자에 최소화 단추를 추가할 경우 아이콘을 그리려면
-//  아래 코드가 필요합니다.  문서/뷰 모델을 사용하는 MFC 응용 프로그램의 경우에는
-//  프레임워크에서 이 작업을 자동으로 수행합니다.
 
 void CObjectTrackingDlg::OnPaint()
 {
 	if (IsIconic())
 	{
-		CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
+		CPaintDC dc(this);
 
 		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
 
-		// 클라이언트 사각형에서 아이콘을 가운데에 맞춥니다.
 		int cxIcon = GetSystemMetrics(SM_CXICON);
 		int cyIcon = GetSystemMetrics(SM_CYICON);
 		CRect rect;
@@ -91,7 +84,6 @@ void CObjectTrackingDlg::OnPaint()
 		int x = (rect.Width() - cxIcon + 1) / 2;
 		int y = (rect.Height() - cyIcon + 1) / 2;
 
-		// 아이콘을 그립니다.
 		dc.DrawIcon(x, y, m_hIcon);
 	}
 	else
@@ -100,8 +92,6 @@ void CObjectTrackingDlg::OnPaint()
 	}
 }
 
-// 사용자가 최소화된 창을 끄는 동안에 커서가 표시되도록 시스템에서
-//  이 함수를 호출합니다.
 HCURSOR CObjectTrackingDlg::OnQueryDragIcon()
 {
 	return static_cast<HCURSOR>(m_hIcon);
@@ -130,12 +120,20 @@ void CObjectTrackingDlg::OnBnClickedBtnPlayVideo()
 			return;
 
 		cv::Mat image, dst;
+		
+		/*char buf[1024];
+		_fullpath(buf, "./cfg/yolov4.cfg", 1024);
+		printf(buf);*/
 
 		// YOLO Load
 		if (!m_pYolo) {
 			m_pYolo = new CYoloModule();
-			if (!m_pYolo->LoadWeight("C:/Project/ObjectTracking/cfg/yolov4.cfg",
-				"C:/Project/ObjectTracking/model/yolov4.weights")) {
+			//if (!m_pYolo->LoadWeight("C:/Project/ObjectTracking/cfg/yolov4.cfg",
+			//	"C:/Project/ObjectTracking/model/yolov4.weights")) {
+			//	AfxMessageBox(_T("YOLO Model Load failed"));
+			//}
+			if (!m_pYolo->LoadWeight("./cfg/yolov4.cfg",
+				"./model/yolov4.weights")) {
 				AfxMessageBox(_T("YOLO Model Load failed"));
 			}
 		}
@@ -167,15 +165,12 @@ void CObjectTrackingDlg::OnBnClickedBtnPlayVideo()
 						t_points[tResults[i].id].erase(t_points[tResults[i].id].begin(), t_points[tResults[i].id].begin() + 3);
 					}
 
-
 					cv::rectangle(dst, tResults[i].box, rColor[tResults[i].id % 255], 2, 8, 0);
 					cv::putText(dst, cv::format("%d", tResults[i].id), tResults[i].box.tl(), 0, 1, cv::Scalar(0, 255, 0), 2);
 					for (int j = 0; j < t_points[tResults[i].id].size(); ++j) {
 						cv::circle(dst, t_points[tResults[i].id][j], 5, rColor[tResults[i].id % 255], -1);
 					}
 				}
-
-				
 			}
 
 			m_pViewer->SetImage(dst);
